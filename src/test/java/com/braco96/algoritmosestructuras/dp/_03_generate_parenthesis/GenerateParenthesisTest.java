b2582a037d578819a19a9976455340bb2a750b0e
braco96 <bravocolladoluis@gmail.com>
2023-03-19 00:00:00 +0000

    Snapshot 1/21 • .gitignore @ 2023-03-19T00:00:00Z

---
+++ src/test/java/com/braco96/algoritmosestructuras/dp/_03_generate_parenthesis/GenerateParenthesisTest.java
@@ -0,0 +1,25 @@
+package com.braco96.algoritmosestructuras.dp._03_generate_parenthesis;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+import com.braco96.algoritmosestructuras.dp._03_generate_parenthesis.GenerateParenthesis;
+
+public class GenerateParenthesisTest {
+
+  @Test
+  public void generateParenthesisTest() {
+    GenerateParenthesis gp = new GenerateParenthesis();
+
+    List<String> result = gp.generateParenthesis(3);
+    assertTrue(result.contains("((()))"));
+    assertTrue(result.contains("(()())"));
+    assertTrue(result.contains("(())()"));
+    assertTrue(result.contains("()(())"));
+    assertTrue(result.contains("()()()"));
+    assertEquals(5, result.size());
+  }
+}
